eb7dc542854a8693163a6b39eac6cf0bee356a50
SuperUser <e.mishechkina@m.tgmu.ru>
2026-01-15 14:35:41 +1000

    Собственные исключения

--- 1.py
+++ 1.py
@@ -1,41 +1,42 @@
 import random
 
+class NotImpLementedException(Exception):
+    ...
+
+
 print("Сегодня замечательная погода для улова!")
-
 enter = input("Планируете много поймать (да/нет)?")
-
 #print('да' == enter)
-
 if 'да' == enter:
     print("Отличного улова!")
 
     choose = input("Какую рыбу вы планируете поймать: карп (1),  лещ (2), щука (3)")
 
-    if '1' == choose:
-        print("Бросай сытный бойл!")
-    elif '2' == choose:
-        print("Нужно создать кормящее пылевое пятно с личинками!")
-    elif '3' == choose:
+    try:
+        if '1' == choose:
+            raise NotImpLementedException("Бросай сытный бойл!")
+        elif '2' == choose:
+            raise NotImpLementedException("Нужно создать кормящее пылевое пятно с личинками!")
+        elif '3' == choose:
 
-        while True:
+            while True:
 
-            print("Прикорма нет, нужно приманить рыбу живцом!")
-
-            s = input("Сколько времени в минутах нужно привлекать рыбу (0-20)?")
-
-            s = int(s)
-
-            win = random.randint(0, 20)
-
-            if s == win:
-                print("Зацепила! Тащите к берегу!")
-            else:
-                print(f"Надо было {win} минут!")
-
-            repeat = input('Попробуете еще раз?')
-
-            if repeat.strip().lower() == 'нет':
-                break
+                print("Прикорма нет, нужно приманить рыбу живцом!")
+                s = input("Сколько времени в минутах нужно привлекать рыбу (0-20)?")
+                # if s.isdigit():
+                #    print('нужно ввести число!')
+                #    continue
+                s = int(s)
+                win = random.randint(0, 20)
+                if s == win:
+                    print("Зацепила! Тащите к берегу!")
+                else:
+                    print(f"Надо было {win} минут!")
+                repeat = input('Попробуете еще раз?')
+                if repeat.strip().lower() == 'нет':
+                    break
+    except NotImpLementedException as err:
+        print(f'Рыбы пока нет: {err}')
 
     print("Значит, просто отдыхайте!")
 
