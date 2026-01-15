9d6b70fe910b13fa0a67e8ec572943be20a39eec
SuperUser <e.mishechkina@m.tgmu.ru>
2026-01-15 12:50:38 +1000

    цикл while с break

--- 2.py
+++ 2.py
@@ -1,9 +1,11 @@
 
-
-repeat = input('Забрасываем снова?')
-
 attempt = 1
-while repeat.lower() != 'нет':
-    print('Забрасывайте! Попытка', attempt)
-    attempt += 1
+while True:
     repeat = input('Забрасываем снова?')
+
+    if repeat == 'нет':
+        break
+
+    print(f' Вы ввели {repeat}, Забрасывайте! Попытка', attempt)
+    attempt += 1
+
