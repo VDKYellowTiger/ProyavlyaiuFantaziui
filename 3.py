befc8a42b255696545353e648b30bfdbe3787579
SuperUser <e.mishechkina@m.tgmu.ru>
2026-01-15 15:38:59 +1000

    Списки, создание

---
+++ 3.py
@@ -0,0 +1,21 @@
+#Списки:
+
+lst = []
+
+#print(lst)
+
+lst = [1, 2, 3]
+
+print(lst)
+
+print(lst[0])
+
+lst[0] = 111
+lst[1] = 22_222
+lst[-1] = 333
+lst[-2] = 'Second element'
+
+print(lst)
+
+
+
