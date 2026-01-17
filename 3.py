6fc60bf8bd71116165e077374e833c558e95fccc
SuperUser <e.mishechkina@m.tgmu.ru>
2026-01-18 01:33:47 +1000

    Методы работы со списком

--- 3.py
+++ 3.py
@@ -1,15 +1,38 @@
 #Списки:
 
-lst = list('строка, которая поделится на буквы')
+lst = [1, 3, 4, 2, 0, 123, 321, 222]
+
+print(lst)
 
 
-crt = (1, 3, 5, 7)
-print(type(crt))
+#Добавление списка
+lst.append(1111)
+lst.append(3_333)
 
-lst1 = list(crt)
-print(type(lst1))
+print(lst)
 
-print (lst, lst1)
+lst2 = ['first', 'second', 'third']
 
+#Расширение списка:
+#lst.extend(lst2)
 
+# lst = lst2
 
+print(lst)
+
+#Сортировка. Метод ничего не возвращает:
+lst.sort(key=lambda x: len(str(x)))
+lst.sort()
+print(f'sorted={lst}')
+
+lst.reverse()
+
+print(f'reverse={lst}')
+
+last = lst.pop()
+
+print(f'last element={last}, list after second pop={lst}')
+
+first = lst.pop(0)
+
+print(f'first element={first}, list after second pop={lst}')
